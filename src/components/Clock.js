0902aa8302d2fe4d1c0619b16573e7b5db6ad9cf
Jeong-eun, Kim <jee824k@gmail.com>
2024-01-28 19:23:24 +0900

    Optimize the rendering Clock logic

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,35 +1,11 @@
 import "./Clock.css";
 
-import { useEffect } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import {
-  currentHourState,
-  currentMinuteState,
-  currentSecondState,
-  currentTimeState,
-} from "../states/datetime";
+import { useSetRecoilState } from "recoil";
 import { tooltipPosState } from "../states/tooltip";
 import ClockHand from "./ClockHand";
 
-const CLOCK_HOURS = 12;
-const CLOCK_MINUTES = 60;
-const CLOCK_SECONDS = 60;
-
 function Clock() {
-  const h = useRecoilValue(currentHourState);
-  const m = useRecoilValue(currentMinuteState);
-  const s = useRecoilValue(currentSecondState);
-  const setCurrentTime = useSetRecoilState(currentTimeState);
   const setTooltipPos = useSetRecoilState(tooltipPosState);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentTime((date) => new Date(date.getTime() + 1000));
-    }, 1000);
-    return () => {
-      clearTimeout(intervalId);
-    };
-  }, [setCurrentTime]);
 
   return (
     <div
@@ -40,9 +16,9 @@ function Clock() {
         setTooltipPos([x, y]);
       }}
     >
-      <ClockHand totalDegree={CLOCK_HOURS} currentDegree={h} />
-      <ClockHand totalDegree={CLOCK_MINUTES} currentDegree={m} />
-      <ClockHand totalDegree={CLOCK_SECONDS} currentDegree={s} />
+      <ClockHand.Hour />
+      <ClockHand.Minute />
+      <ClockHand.Second />
     </div>
   );
 }
--- src/components/ClockHand.js
+++ src/components/ClockHand.js
@@ -1,6 +1,15 @@
 import "./ClockHand.css";
 
-function ClockHand({ totalDegree, currentDegree }) {
+import { useEffect } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import {
+  currentHourState,
+  currentMinuteState,
+  currentSecondState,
+  currentTimeState,
+} from "../states/datetime";
+
+function ClockHandRoot({ currentDegree, totalDegree }) {
   const deg = (currentDegree / totalDegree) * 360;
 
   return (
@@ -12,5 +21,51 @@ function ClockHand({ totalDegree, currentDegree }) {
     />
   );
 }
+
+function ClockHandHour() {
+  const hour = useRecoilValue(currentHourState);
+
+  return <ClockHand currentDegree={hour} totalDegree={12} />;
+}
+
+function ClockHandMinute() {
+  const minute = useRecoilValue(currentMinuteState);
+
+  return <ClockHand currentDegree={minute} totalDegree={60} />;
+}
+
+function ClockHandSecond() {
+  const second = useRecoilValue(currentSecondState);
+  const setCurrentTime = useSetRecoilState(currentTimeState);
+
+  useEffect(() => {
+    // 현재 시간에서 다음 초까지 차이나는 밀리초 이후, 첫 번째 상태 업데이트를 한다.
+    // 예를 들어 5.6초에서 첫 번째 렌더한 경우 0.4초 후에 6초로 업데이트한다.
+    const firstTimeout = 1000 - (new Date().getTime() % 1000);
+
+    let intervalId;
+    const timeoutId = setTimeout(() => {
+      setCurrentTime(new Date());
+
+      // 첫 번째 상태 업데이트 후 1초마다 업데이트한다.
+      intervalId = setInterval(() => {
+        setCurrentTime(new Date());
+      }, 1000);
+    }, firstTimeout);
+
+    return () => {
+      clearTimeout(timeoutId);
+      if (intervalId !== undefined) clearInterval(intervalId);
+    };
+  }, [setCurrentTime]);
+
+  return <ClockHand currentDegree={second} totalDegree={60} />;
+}
+
+const ClockHand = Object.assign(ClockHandRoot, {
+  Hour: ClockHandHour,
+  Minute: ClockHandMinute,
+  Second: ClockHandSecond,
+});
 
 export default ClockHand;
